7b0894ff434ca41e1884980a7431a36bc10a36b0
Luke <sejun910@naver.com>
2022-08-24 10:59:22 +0900

    'start-view'

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,47 @@
-import '../styles/globals.css'
+import React from "react";
+React.useLayoutEffect = React.useEffect;
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import "react-perfect-scrollbar/dist/css/styles.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "../public/assets/css/style.css";
+import "../public/assets/css/widgets.css";
+import "../public/assets/css/responsive.css";
+import "metismenujs/dist/metismenujs.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.WOW = require("wowjs");
+    }
+
+    new WOW.WOW().init();
+
+    const hasGridClass = document.querySelector(".grid-sizer");
+
+    if (hasGridClass != null) {
+      const xyz = require("masonry-layout");
+      var msnry = new xyz(".grid", {
+        itemSelector: ".grid-item",
+        columnWidth: ".grid-sizer",
+      });
+    }
+
+    const handleRouteChangeError = (err, url) => {
+      if (err.cancelled) {
+        console.log(`Route to ${url} was cancelled!`);
+      }
+    };
+
+    return () => {
+      router.events.off("routeChangeError", handleRouteChangeError);
+    };
+  }, []);
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
